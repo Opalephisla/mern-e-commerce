efd03e3e452284c76dddb920a8369be3efa7a200
Evano Lilian <lilian.evano@gmail.com>
2022-09-08 16:57:59 +0200

    homescreen + cart wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,52 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HomeScreen, ProductScreen } from './screens'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Badge, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { Store } from './Store'
 
 function App() {
+  const { state } = useContext(Store)
+  const { cart } = state
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,123 @@
+import axios from 'axios'
+import React, { useContext, useEffect, useReducer } from 'react'
+import { Badge, Button, Card, Col, ListGroup, Row } from 'react-bootstrap'
+import { Helmet } from 'react-helmet-async'
+import { useParams } from 'react-router-dom'
+import { LoadingBox, MessageBox, Rating } from '../components'
+import { Store } from '../Store'
+import { getError } from '../utils'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true }
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false }
+    case 'FETCH_FAIL':
+      console.log(action.payload)
+      return { ...state, loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+const ProductScreen = () => {
+  const params = useParams()
+  const { slug } = params
+
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  })
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' })
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`)
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
+      }
+    }
+    fetchData()
+  }, [slug])
+
+  const { state, dispatch: ctxDispatch } = useContext(Store)
+  const addToCartHandler = () => {
+    ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } })
+  }
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
+    (console.log(error, product),
+    (
+      <div>
+        <Row>
+          <Col md={6}>
+            <img className="img-large" src={product.image} alt={product.name} />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Helmet>
+                  <title>{product.name}</title>
+                </Helmet>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  rating={product.rating}
+                  numReviews={product.numReviews}
+                ></Rating>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Description:
+                <p>{product.description}</p>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <Card.Body>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>${product.price}</Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.countInStock > 0 ? (
+                          <Badge bg="success">In Stock</Badge>
+                        ) : (
+                          <Badge bg="danger">Unavailable</Badge>
+                        )}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <div className="d-grid">
+                        <Button onClick={addToCartHandler} variant="primary">
+                          Add to Cart
+                        </Button>
+                      </div>
+                    </ListGroup.Item>
+                  )}
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    ))
+  )
+}
+
+export default ProductScreen
