4b75c2e6d7d3030deefdbe6e97472095846380e6
Opalephisla <35504111+Opalephisla@users.noreply.github.com>
2022-09-10 19:59:03 +0200

    usermodel and user seed data

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs"
+
 const data = {
+  users: [
+    {
+      name: "Lilian",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Bob",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
