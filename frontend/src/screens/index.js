4edf5bfa81df18422f92338d0b1fb19e5de7a475
Evano Lilian <lilian.evano@gmail.com>
2022-09-23 10:45:39 +0200

    typo modification

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -1,7 +1,7 @@
 export { default as HomeScreen } from './HomeScreen'
 export { default as ProductScreen } from './ProductScreen'
 export { default as CartScreen } from './CartScreen'
-export { default as SigninScreen } from './SigninScreen'
+export { default as SigninScreen } from './SignInScreen'
 export { default as SignupScreen } from './SignupScreen'
 export { default as ShippingAddressScreen } from './ShippingAddressScreen'
 export { default as PaymentMethodScreen } from './PaymentMethodScreen'
