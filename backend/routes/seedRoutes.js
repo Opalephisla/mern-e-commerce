e8d33d2a830fcc6f38405d3bb0b8b5672860fa24
Evano Lilian <lilian.evano@gmail.com>
2022-09-23 10:56:54 +0200

    seed routes modification

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -6,9 +6,9 @@ import User from '../models/userModel.js'
 const seedRouter = express.Router()
 
 seedRouter.get('/', async (req, res) => {
-  await Product.deleteMany({})
+  await Product.remove({})
   const createdProducts = await Product.insertMany(data.products)
-  await User.deleteMany({})
+  await User.remove({})
   const createdUsers = await User.insertMany(data.users)
   res.send({ createdProducts, createdUsers })
 })
